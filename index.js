9c9c4ba28336d6eef5dbc1e87ca6bfb90e809e4f
CryptoKiddd <avaliani1232@gmail.com>
2023-08-17 01:48:03 +0400

    particle system

--- index.js
+++ index.js
@@ -12,12 +12,32 @@ class Particle{
         this.effect = effect;
         this.x = Math.floor(Math.random() * this.effect.width);
         this.y = Math.floor(Math.random() * this.effect.height);
+        this.speedX = Math.random() *5 -2.5
+        this.speedY = Math.random()  *5 -2.5
+        this.history = [ {x:this.x, y:this.y}];
+        this.maxLength = 60;
+        this.angle = 0 
 
 
     }
     draw(context){
-        context.fillRect(this.x,this.y,10,10)
+        context.fillRect(this.x,this.y,5,5);
+        context.beginPath();
+        context.moveTo(this.history[0].x, this.history[0].y);
+        for (let i = 0; i < this.history.length;i++){
+            context.lineTo(this.history[i].x, this.history[i].y)
+        }
+        context.stroke()
 
+    }
+    update(){
+        this.angle += 0.5
+        this.x += this.speedX + Math.sin(this.angle)
+        this.y += this.speedY +  Math.cos(this.angle)
+        this.history.push({x:this.x,y:this.y})
+        if(this.history.length > this.maxLength){
+            this.history.shift()
+        }
     }
 
 
@@ -27,7 +47,8 @@ class Effect{
         this.width = width;
         this.height = height;
         this.particles = [];
-        this.numOfParticles = 50
+        this.numOfParticles = 50;
+        this.init()
 
 
     }
@@ -39,13 +60,19 @@ class Effect{
     }
     render(ctx){
         this.particles.forEach(particle=>{
-            particle.draw(ctx)
+            particle.draw(ctx);
+            particle.update();
         })
     }
 
 }
 
 const effect = new Effect(canvas.width,canvas.height);
-effect.init();
-effect.render(ctx)
-console.log(effect)
+
+
+function animate(){
+    ctx.clearRect(0,0,canvas.width,canvas.height)
+    effect.render(ctx);
+    requestAnimationFrame(animate)
+}
+animate()
