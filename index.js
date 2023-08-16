6d52a2fa79b49abcbb9d136c9aa41fa06a566ecb
CryptoKiddd <avaliani1232@gmail.com>
2023-08-17 00:56:44 +0400

    init

---
+++ index.js
@@ -0,0 +1,7 @@
+const canvas = document.getElementById('canvas1')
+const ctx = canvas.getContext('2d')
+canvas.height = window.innerHeight;
+canvas.width = window.innerWidth;
+
+
+ctx.fillStyle = 'white'
