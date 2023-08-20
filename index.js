2df2ded350987afa4c9d43c662f5165dc7b110db
CryptoKiddd <avaliani1232@gmail.com>
2023-08-21 03:41:23 +0400

    merged with spongebob

--- index.js
+++ index.js
@@ -155,6 +155,152 @@ setTimeout(()=>{
     canvas.remove()
 },7500)
 /////end of website enter visuals
+setTimeout(()=>{
 
-const canvas2 = document.getElementById('canvas2')
-const ctx2 = canvas2.getContext('2d')
+    
+       const imageCanvas  = document.getElementById('canvas2')
+       const imageCanvasCtx = imageCanvas.getContext('2d')
+       const btn = document.getElementById('btn')
+   
+       const image = document.getElementById('image')
+      
+   
+       imageCanvas.width = window.innerWidth;
+       imageCanvas.height = window.innerHeight;
+   
+   
+       class imageParticle{
+           constructor(effect,x,y,color){
+               this.effect = effect
+               this.x = (Math.random() * this.effect.width - this.effect.width / 2) * 5
+               this.y = (Math.random() * this.effect.width - this.effect.width / 2) * 5
+               this.originX = Math.floor(x)
+               this.originY =Math.floor(y)
+               this.size = 2
+               this.vx = 0
+               this.vy =  0
+               this.color = color
+               this.ease = 0.02;
+               this.dx = 0
+               this.dy = 0
+               this.distance =0
+               this.force = 0
+               this.angle = 0
+               this.friction = 0.45
+           }
+   
+           draw(context){
+               context.fillStyle=this.color
+               context.fillRect(this.x,this.y,this.size,this.size)
+           }
+           update(){
+               this.dx = this.effect.mouse.x - this.x
+               this.dy = this.effect.mouse.y - this.y
+               this.distance = this.dx * this.dx + this.dy *this.dy
+               this.force = -this.effect.mouse.radius / this.distance
+   
+               if(this.distance <this.effect.mouse.radius ){
+                   this.angle = Math.atan2(this.dy,this.dx)
+                   this.vx += this.force * Math.cos(this.angle)
+                   this.vy += this.force * Math.sin(this.angle)
+               }
+   
+               this.x += (this.vx *=this.friction)  +(this.originX - this.x) * this.ease
+               this.y +=  (this.vy*=this.friction ) +(this.originY - this.y)* this.ease
+           }
+           warp(){
+               this.x = Math.random() * this.effect.width 
+               this.y = Math.random() * this.effect.height
+               this.ease = 0.07
+           }
+   
+       }
+   
+   
+   
+       class imageEffect{
+           constructor(width,height){
+               this.width = width;
+               this.height = height;
+               this.amount = 100
+               this.particles = [];
+               this.image = document.getElementById('image');
+               this.centerX = this.width * 0.5;
+               this.centerY = this.height * 0.5;
+               this.x = this.centerX - this.image.width * 0.5
+               this.y = this.centerY - this.image.height * 0.5;
+               this.gap = 3;
+               this.mouse = {
+                   radius:10000,
+                   x:undefined,
+                   y:undefined
+               }
+               window.addEventListener('mousemove',(e)=>{
+                   this.mouse.x = e.x;
+                   this.mouse.y = e.y
+   
+               })
+   
+           }
+           init(imageCanvasCtx){
+             imageCanvasCtx.drawImage(this.image, this.x, this.y)
+               const pixels = imageCanvasCtx.getImageData(0,0,this.width,this.height).data
+               for(let y = 0; y < this.height; y +=this.gap){
+                   for(let x = 0; x < this.width; x +=this.gap){
+                      const index = (y * this.width + x) * 4;
+                      const red = pixels[index] 
+                      const green = pixels[index + 1] 
+                      const blue = pixels[index + 2] 
+                      const alpha = pixels[index + 3] 
+                      const color = `rgb(${red},${green},${blue})`
+   
+                      if(alpha > 0){
+                       this.particles.push(new imageParticle(this,x,y,color))
+                      }
+                   }
+               }
+               
+           }
+           draw(imageCanvasCtx){
+               this.particles.forEach(particle=>particle.draw(imageCanvasCtx));
+               
+               
+           }
+           update(){
+            
+               this.particles.forEach(particle=>particle.update());
+           }
+           warp(){
+               this.particles.forEach(particle=>particle.warp());
+           }
+   
+       }
+       const imageEffectInstance = new imageEffect(imageCanvas.width,imageCanvas.height);
+       
+       imageEffectInstance.init(imageCanvasCtx)
+       imageEffectInstance.draw(imageCanvasCtx);
+       imageEffectInstance.update()
+       
+      
+   
+       function animate(){
+           imageCanvasCtx.clearRect(0,0,imageCanvas.width,imageCanvas.height)
+           imageEffectInstance.draw(imageCanvasCtx);
+           imageEffectInstance.update()
+           requestAnimationFrame(animate)
+       }
+       animate()
+   
+       btn.addEventListener('click',function(){
+           imageEffectInstance.warp()
+   
+       })
+       document.querySelector('.main-heading').textContent='Hello I am Different Web Developer'
+   
+       
+   
+   
+   
+    
+},7501)
+ 
